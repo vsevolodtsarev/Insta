64f37664c84ef8aabf47492cfa538aa70cb8c90c
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-16 14:41:23 +0300

    Initial Commit

---
+++ Insta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Insta
+//
+//  Created by Всеволод Царев on 16.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Insta/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by Всеволод Царев on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
