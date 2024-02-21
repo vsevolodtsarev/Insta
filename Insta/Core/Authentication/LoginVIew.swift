6b5f6a9fa51fcf330496cf4f08a2739ab2380912
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-21 16:00:09 +0300

    Create LoginView

---
+++ Insta/Core/Authentication/LoginVIew.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVIew.swift
+//  Insta
+//
+//  Created by Всеволод Царев on 21.02.2024.
+//
+
+import SwiftUI
+
+struct LoginVIew: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    LoginVIew()
+}
