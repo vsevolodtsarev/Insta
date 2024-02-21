174153706402951c100374fa6a50487556c8263f
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-21 21:12:32 +0300

    Update navigation flow

--- Insta/Core/Authentication/AddEmailView.swift
+++ Insta/Core/Authentication/AddEmailView.swift
@@ -9,45 +9,53 @@ import SwiftUI
 
 struct AddEmailView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var isShowCreateUsernameScreen = false
     @State private var email = ""
     
     var body: some View {
-        VStack(spacing: 12) {
-            Text("Add your email")
-                .font(.title2)
-                .fontWeight(.bold)
-                .padding(.top)
-            
-            Text("You'll use this email to sign in to your account")
-                .font(.footnote)
-                .foregroundStyle(.gray)
-                .multilineTextAlignment(.center)
+        NavigationStack {
+            VStack(spacing: 12) {
+                Text("Add your email")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.top)
+                
+                Text("You'll use this email to sign in to your account")
+                    .font(.footnote)
+                    .foregroundStyle(.gray)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 24)
+                
+                InstaInputView(text: $email,
+                               placeholder: "Enter your email",
+                               isSecureField: false)
+                .textInputAutocapitalization(.never)
+                .keyboardType(.emailAddress)
                 .padding(.horizontal, 24)
-            
-            InstaInputView(text: $email,
-                           placeholder: "Enter your email",
-                           isSecureField: false)
-            .textInputAutocapitalization(.never)
-            .keyboardType(.emailAddress)
-            .padding(.horizontal, 24)
-            .padding(.top)
-            
-            InstaButtonView(text: "Next",
-                            isAuthButton: true) {
-                print("to new name")
-            }
-                            .padding(.top)
-        }
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                SFSymbolsImage.backButton
-                    .imageScale(.large)
-                    .onTapGesture {
-                        dismiss()
+                .padding(.top)
+                
+                NavigationLink(isActive: $isShowCreateUsernameScreen) {
+                    CreateUsernameVIew()
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    InstaButtonView(text: "Next",
+                                    isAuthButton: true) {
+                        isShowCreateUsernameScreen.toggle()
                     }
+                                    .padding(.top)
+                }
             }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    SFSymbolsImage.backButton
+                        .imageScale(.large)
+                        .onTapGesture {
+                            dismiss()
+                        }
+                }
+            }
+            Spacer()
         }
-        Spacer()
     }
 }
 
