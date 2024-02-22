d42abe7a82df5a22de6e2758babd6df051f2d958
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-22 20:19:00 +0300

    Add data to model

--- Insta/Model/User.swift
+++ Insta/Model/User.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    let email: String
+    var username: String
+    var fullname: String?
+    var bio: String?
+    var profileImageUrl: String?
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: UUID().uuidString,
+              email: "test@gmail.com",
+              username: "mockuser1",
+              fullname: "Mock User1",
+              bio: "user1 mock bio",
+              profileImageUrl: nil),
+        .init(id: UUID().uuidString,
+              email: "test2@gmail.com",
+              username: "mockuser2",
+              fullname: nil,
+              bio: "user2 mock bio",
+              profileImageUrl: "mockAvatar"),
+        .init(id: UUID().uuidString,
+              email: "test3@gmail.com",
+              username: "mockuser3",
+              fullname: "Mock User3",
+              bio: "user3 mock bio",
+              profileImageUrl: "mockAvatar"),
+        .init(id: UUID().uuidString,
+              email: "test4@gmail.com",
+              username: "mockuser4",
+              fullname: "Mock User4",
+              bio: "user4 mock bio",
+              profileImageUrl: "mockAvatar"),
+        .init(id: UUID().uuidString,
+              email: "test5@gmail.com",
+              username: "mockuser5",
+              fullname: "Mock User5",
+              bio: "user5 mock bio",
+              profileImageUrl: "mockAvatar")
+    ]
+}
