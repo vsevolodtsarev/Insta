b0d72f33b4ee2adf1216d8c24eea336ea3d31500
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-24 21:54:20 +0300

    Add to MainTabView tab indexes

--- Insta/Core/TabBar/MainTabView.swift
+++ Insta/Core/TabBar/MainTabView.swift
@@ -8,22 +8,44 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedIndex) {
             FeedView()
+                .onAppear {
+                    selectedIndex = 0
+                }
                 .tabItem { SFSymbolsImage.feed }
+                .tag(0)
             
             SearchView()
+                .onAppear {
+                    selectedIndex = 1
+                }
                 .tabItem { SFSymbolsImage.search }
+                .tag(1)
             
-            Text("Upload Post")
+            UploadPostView(tabIndex: $selectedIndex)
+                .onAppear {
+                    selectedIndex = 2
+                }
                 .tabItem { SFSymbolsImage.uploadPost }
+                .tag(2)
             
             Text("Notifications")
+                .onAppear {
+                    selectedIndex = 3
+                }
                 .tabItem { SFSymbolsImage.notifications }
+                .tag(3)
             
             CurrentUserProfileView(user: User.MOCK_USERS[1])
+                .onAppear {
+                    selectedIndex = 4
+                }
                 .tabItem { SFSymbolsImage.profile }
+                .tag(4)
         }
     }
 }
