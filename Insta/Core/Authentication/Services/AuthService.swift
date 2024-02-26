61c2088a550d9199f0aa75d34f45fe5bc97a73f8
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-26 16:14:03 +0300

    Create AuthService(work in progress...)

---
+++ Insta/Core/Authentication/Services/AuthService.swift
@@ -0,0 +1,36 @@
+//
+//  AuthService.swift
+//  Insta
+//
+//  Created by Всеволод Царев on 26.02.2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+final class AuthService {
+    
+    @Published private var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        userSession = Auth.auth().currentUser
+    }
+    
+    func createUser(email: String, password: String, username: String) async throws {
+        
+    }
+    
+    func login(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signOut() {
+        
+    }
+}
