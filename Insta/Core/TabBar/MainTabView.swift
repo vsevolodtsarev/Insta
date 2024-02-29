7f7cd4d62ce796655b507361604125e7374d50dd
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-29 22:36:56 +0300

    Implement User from Firebase

--- Insta/Core/TabBar/MainTabView.swift
+++ Insta/Core/TabBar/MainTabView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct MainTabView: View {
+    let user: User
     @State private var selectedIndex = 0
     
     var body: some View {
@@ -40,7 +41,7 @@ struct MainTabView: View {
                 .tabItem { SFSymbolsImage.notifications }
                 .tag(3)
             
-            CurrentUserProfileView(user: User.MOCK_USERS[1])
+            CurrentUserProfileView(user: user)
                 .onAppear {
                     selectedIndex = 4
                 }
@@ -51,5 +52,5 @@ struct MainTabView: View {
 }
 
 #Preview {
-    MainTabView()
+    MainTabView(user: User.MOCK_USERS[0])
 }
