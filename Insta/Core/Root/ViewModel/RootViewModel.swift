025aa6253bfa80a7b7137b431f4af99f1344f422
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-02-29 22:37:37 +0300

    Create User Publisher

--- Insta/Core/Root/ViewModel/RootViewModel.swift
+++ Insta/Core/Root/ViewModel/RootViewModel.swift
@@ -14,15 +14,22 @@ final class RootViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
     
     @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
     
     init() {
         setupSubscribers()
     }
     
     private func setupSubscribers() {
-        let _ = authService.$userSession.sink { [weak self] userSession in
+        authService.$userSession.sink { [weak self] userSession in
             guard let self else { return }
             self.userSession = userSession
+        }
+            .store(in: &cancellables)
+        
+        authService.$currentUser.sink { [weak self] currentUser in
+            guard let self else { return }
+            self.currentUser = currentUser
         }
             .store(in: &cancellables)
     }
