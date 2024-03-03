ba414ea4a13dcae0026474c0cb81c3f97a603322
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-03-03 17:40:56 +0300

    Add EditProfileView

---
+++ Insta/Core/Profile/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Insta
+//
+//  Created by Всеволод Царев on 03.03.2024.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
